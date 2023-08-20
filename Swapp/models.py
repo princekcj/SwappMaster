e4f5d31e5ed369bcbbfee9d1eb396cc5e2466b39
princekcj <cjbizent@Gmail.com>
2023-08-20 19:41:31 +0100

    tese

--- Swapp/models.py
+++ Swapp/models.py
@@ -15,6 +15,7 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     location = db.Column(db.String(120), unique=False, nullable=False)
+    is_superuser = db.Column(db.Boolean, nullable=False, default= False)
     phone = db.Column(db.String(120), unique=True, nullable=False)
     user_events = db.relationship('UserEvents', backref='UserEvents', lazy=True)
     password = db.Column(db.String(60), nullable=False)
